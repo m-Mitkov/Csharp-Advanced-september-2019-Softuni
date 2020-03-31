e6fcb4b5c37ce6bb4a90210f21d0f2ceb5fe4d87
Martin Mitkov <m.mitkov07@gmail.com>
2020-03-31 21:48:57 +0200

    MultidimensionalArrays/Exercise/miner/

--- MultidimensionalArrays/Exercise/maximalSum/Program.cs
+++ MultidimensionalArrays/Exercise/maximalSum/Program.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace maximalSum
+namespace maximalSum1
 {
     class Program
     {
@@ -18,42 +18,45 @@ namespace maximalSum
 
             int[,] matrix = new int[rows, cols];
 
-            int sumMatrixRow = 3;
-            int subMatrixCol = 3;
-
             WriteMatrix(matrix);
 
-            int biggestSum = int.MinValue;
+            int subMatrixRow = 3;
+            int subMatrixCol = 3;
+
+            int maxSumOfSubRowsCols = 0;
             int maxRow = 0;
             int maxCol = 0;
 
-            for (int row = 0; row <= matrix.GetLength(0) - sumMatrixRow; row++)
+            for (int row = 0; row < matrix.GetLength(0) - subMatrixRow + 1; row++)
             {
-
-                for (int col = 0; col <= matrix.GetLength(1) - subMatrixCol; col++)
+                for (int col = 0; col < matrix.GetLength(1) - subMatrixCol + 1; col++)
                 {
-                    int currentSum = 0;
+                    int currentBestSum = 0;
 
-                    for (int subRow = 0; subRow < sumMatrixRow; subRow++)
+                    for (int subRow = 0; subRow < subMatrixRow; subRow++)
                     {
                         for (int subCol = 0; subCol < subMatrixCol; subCol++)
                         {
-                            currentSum += matrix[row + subRow, col + subCol];
+                            currentBestSum += matrix[subRow + row, subCol + col];
                         }
                     }
 
-                    if (currentSum > biggestSum)
+                    if (currentBestSum > maxSumOfSubRowsCols)
                     {
-                        biggestSum = currentSum;
+                        maxSumOfSubRowsCols = currentBestSum;
                         maxRow = row;
                         maxCol = col;
                     }
                 }
             }
 
-            Console.WriteLine($"Sum = {biggestSum}");
+            Console.WriteLine($"Sum = {maxSumOfSubRowsCols}");
+            PrintMatrix(matrix, subMatrixRow, subMatrixCol, maxRow, maxCol);
+        }
 
-            for (int row = 0; row < sumMatrixRow; row++)
+        public static void PrintMatrix(int[,] matrix, int subMatrixRow, int subMatrixCol, int maxRow, int maxCol)
+        {
+            for (int row = 0; row < subMatrixRow; row++)
             {
                 for (int col = 0; col < subMatrixCol; col++)
                 {
@@ -62,15 +65,14 @@ namespace maximalSum
                 Console.WriteLine();
             }
         }
-
-        private static void WriteMatrix(int[,] matrix)
+        public static void WriteMatrix(int[,] matrix)
         {
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 int[] input = Console.ReadLine()
-                    .Split()
-                    .Select(int.Parse)
-                    .ToArray();
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
 
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
