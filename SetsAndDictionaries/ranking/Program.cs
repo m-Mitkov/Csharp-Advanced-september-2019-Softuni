8127afba5d51fba789af6d2e7b619f385040f668
Martin Mitkov <m.mitkov07@gmail.com>
2020-04-13 20:20:48 +0200

    13/20/2020

---
+++ SetsAndDictionaries/ranking/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contestPassword = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end of contests")
+                {
+                    break;
+                }
+
+                string[] inputArgs = input.Split(":");
+                string course = inputArgs[0];
+                string password = inputArgs[1];
+
+                if (!contestPassword.ContainsKey(course))
+                {
+                    contestPassword.Add(course, password);
+                }
+            }
+
+            var studentsDatabase = new Dictionary<string, Dictionary<string, int>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end of submissions")
+                {
+                    break;
+                }
+
+                string[] inputArgs = input.Split("=>");
+
+                string contest = inputArgs[0];
+                string password = inputArgs[1];
+                string username = inputArgs[2];
+                int points = int.Parse(inputArgs[3]);
+
+                if (contestPassword.ContainsKey(contest) && contestPassword[contest] == password)
+                {
+                    if (!studentsDatabase.ContainsKey(username))
+                    {
+                        studentsDatabase.Add(username, new Dictionary<string, int>());
+                    }
+
+                    if (!studentsDatabase[username].ContainsKey(contest))
+                    {
+                        studentsDatabase[username].Add(contest, points);
+                    }
+
+                    if (studentsDatabase[username][contest] < points)
+                    {
+                        studentsDatabase[username][contest] = points;
+                    }
+                }
+            }
+
+            var topStudent = studentsDatabase
+                .OrderByDescending(x => x.Value.Sum(s => s.Value))
+                .FirstOrDefault();
+
+            Console.WriteLine($"Best candidate is {topStudent.Key} with total {topStudent.Value.Sum(x => x.Value)} points.");
+
+            Console.WriteLine($"Ranking:");
+
+            foreach (var (username, contest) in studentsDatabase.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(username);
+
+                foreach (var (course, points) in contest.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {course} -> {points}");
+                }
+            }
+        }
+    }
+}
