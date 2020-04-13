86c846436b5978937ab04877e40345d7d3a0435d
Martin Mitkov <m.mitkov07@gmail.com>
2020-04-13 11:48:26 +0200

    13/04/2020 commit

---
+++ SetsAndDictionaries/countSymbols/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace countSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> countSymbols = new SortedDictionary<char, int>();
+
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+
+                if (!countSymbols.ContainsKey(currentSymbol))
+                {
+                    countSymbols.Add(currentSymbol, 1);
+                    continue;
+                }
+
+                countSymbols[currentSymbol]++;
+            }
+
+            foreach (var (@char, count) in countSymbols)
+            {
+                Console.WriteLine($"{@char}: {count} time/s");
+            }
+        }
+    }
+}
---
+++ SetsAndDictionaries/theVlogger/theVlogger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace theVLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vlogers = new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+
+            while (true)
+            {
+                string[] comand = Console.ReadLine()
+                    .Split(" ");
+
+                if (comand[0] == "Statistics")
+                {
+                    break;
+                }
+                string toDo = comand[1];
+
+                if (toDo == "joined")
+                {
+                    string vloger = comand[0];
+
+                    if (!vlogers.ContainsKey(vloger))
+                    {
+                        vlogers.Add(vloger, new Dictionary<string, SortedSet<string>>());
+                        vlogers[vloger].Add("followers", new SortedSet<string>());
+                        vlogers[vloger].Add("following", new SortedSet<string>());
+                    }
+                }
+
+                else if (toDo == "followed")
+                {
+                    string firstVloger = comand[0];
+                    string followedVloger = comand[2];
+
+                    if (vlogers.ContainsKey(firstVloger) && vlogers.ContainsKey(followedVloger)
+                        && firstVloger != followedVloger)
+                    {
+                        vlogers[firstVloger]["following"].Add(followedVloger);
+                        vlogers[followedVloger]["followers"].Add(firstVloger);
+                    }
+                }
+            }
+
+            vlogers = vlogers
+                 .OrderByDescending(x => x.Value["followers"].Count)
+                .ThenBy(x => x.Value["following"].Count)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            Console.WriteLine($"The V-Logger has a total of {vlogers.Count} vloggers in its logs.");
+
+            int count = 0;
+            foreach (var (vloger, people) in vlogers)
+            {
+                Console.WriteLine($"{++count}. {vloger} : {people["followers"].Count} followers, " +
+                    $"{people["following"].Count} following");
+
+                if (count == 1)
+                {
+                    foreach (var follower in people["followers"])
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+            }
+        }
+    }
+}
