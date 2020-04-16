153b018c97486c9d90dfb5f631a7d80e60b83060
Martin Mitkov <m.mitkov07@gmail.com>
2020-04-16 22:10:23 +0200

    16/04/2020

---
+++ FunctionalPrograming/ListOfPredicates/listOfPredicates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace listOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rangeLength = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, bool> checkIfNumberIsDivisible = (n, d) => n % d == 0;
+
+            for (int i = 1; i <= rangeLength; i++)
+            {
+                bool isDivisible = true;
+
+                foreach (var number in dividers)
+                {
+                    if (!checkIfNumberIsDivisible(i, number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
