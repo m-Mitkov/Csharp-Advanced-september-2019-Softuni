1bac27fa9e58299d933191f06b9dd42083b0319f
Martin Mitkov <m.mitkov07@gmail.com>
2020-04-18 10:10:52 +0200

    18/04/2020 commit

---
+++ FunctionalPrograming/partyReservationFilterModule/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace partyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> filtersToApply = new List<string>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(";", 2)
+                    .ToArray();
+
+                if (input[0] == "Print")
+                {
+                    break;
+                }
+
+                string comand = input[0];
+                string filterType = input[1];
+
+                if (comand == "Add filter")
+                {
+                    filtersToApply.Add(filterType);
+                }
+
+                else if (comand == "Remove filter")
+                {
+                    filterType.Replace(filterType, null);
+                }
+            }
+
+            foreach (var filters in filtersToApply)
+            {
+                string[] currentFilter = filters
+                    .Split(";");
+
+                string filterType = currentFilter[0];
+                string filterParameter = currentFilter[1];
+
+                Func<string, string, bool> filter = FunctionType(filterType);
+
+                guests = guests
+                    .Where(x => !filter(x, filterParameter))
+                    .ToList();
+            }
+
+            Console.WriteLine(string.Join(" ", guests));
+        }
+
+        static Func<string, string, bool> FunctionType(string filterType)
+        {
+            if (filterType == "Starts with")
+            {
+                return (name, parameter) => name.StartsWith(parameter);
+            }
+            else if (filterType == "Ends with")
+            {
+                return (name, parameter) => name.EndsWith(parameter);
+            }
+            else if (filterType == "Length")
+            {
+                return (name, parameter) => name.Length == int.Parse(parameter);
+            }
+            else if (filterType == "Contains")
+            {
+                return (name, parameter) => name.Contains(parameter);
+            }
+            return null;
+        }
+    }
+}
---
+++ FunctionalPrograming/predicateParty/predicateParty.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace predicateParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine()
+                .Split(" ")
+                .ToList();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "Party!")
+                {
+                    break;
+                }
+
+                string comand = input[0];
+                string criteria = input[1];
+
+                if (criteria == "StartsWith")
+                {
+                    string basedCriteria = input[2];
+
+                    Predicate<string> checkIfStartWith = name => name.StartsWith(basedCriteria);
+                    guests = DoubleOrRemove(guests, comand, checkIfStartWith);
+                }
+
+                else if (criteria == "EndsWith")
+                {
+                    string basedCriteria = input[2];
+                    Predicate<string> endsWith = name => name.EndsWith(basedCriteria);
+                    guests = DoubleOrRemove(guests, comand, endsWith);
+                }
+
+                else if (criteria == "Length")
+                {
+                    int lenghtCriteria = int.Parse(input[2]);
+                    Predicate<string> lenghtIsCorrect = name => name.Length == lenghtCriteria;
+                    guests = DoubleOrRemove(guests, comand, lenghtIsCorrect);
+                }
+            }
+
+            Console.WriteLine(guests.Any() ? $"{string.Join(", ", guests)} are going to the party!" : 
+                "Nobody is going to the party!");
+        }
+
+        static List<string> DoubleOrRemove(List<string> guests, string comand,Predicate<string> filter)
+        {
+            List<string> newList = new List<string>();
+
+            foreach (var guest in guests)
+            {
+                if (filter(guest))
+                {
+                    if (comand == "Double")
+                    {
+                        newList.Add(guest);
+                    }
+
+                    else if (comand == "Remove")
+                    {
+                        continue;
+                    }
+                }
+                newList.Add(guest);
+            }
+
+            return newList;
+        }
+    }
+}
