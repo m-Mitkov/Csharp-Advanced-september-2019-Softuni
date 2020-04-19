d3ca424a686eda4c1430785a39a7a05adcb5f4fb
Martin Mitkov <m.mitkov07@gmail.com>
2020-04-19 11:32:16 +0200

    19/04/2020

---
+++ FunctionalPrograming/triFunction/triFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace triFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parameterNumber = int.Parse(Console.ReadLine());
+
+            Func<string, char[]> toCharArray = x => x.ToCharArray();
+            Func<char, int> castChar = x => (int)x;
+            Func<string, bool> result = x => toCharArray(x).Select(castChar).Sum() >= parameterNumber;
+
+            Console.WriteLine
+                (Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault(result));
+        }
+    }
+}
